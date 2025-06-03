348a087e280ba61e1df19030e6b1668fe6928fd6
Gaurisatpute77 <Gaurisatpute2002@gmail.com>
2025-06-04 02:04:03 +0530

    Add files via upload

---
+++ WhileVSDo_WhileLoop.java
@@ -0,0 +1,23 @@
+package JAVAPackage;
+
+public class WhileVSDo_WhileLoop {
+
+	public static void main(String[] args) {
+
+		// While loop and do while loop comparison
+		int i= 10;
+		while(i<=50) {
+
+			System.out.println(i);
+			i+=10;
+		}
+
+		int a=5;
+		do {
+			System.out.println(a);
+			a+=5;
+		}while(a<=100);
+
+	}
+
+}
